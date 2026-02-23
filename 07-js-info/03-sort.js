7a6202a1733ea885d876e0cd95b71ede3383b2ab
Gody <250132528+matecg@users.noreply.github.com>
2026-02-23 14:28:48 -0300

    Add solution for exercise 05, to copy and sort an array

--- 07-js-info/03-sort.js
+++ 07-js-info/03-sort.js
@@ -2,8 +2,18 @@ function sortDescending(arr) {
     arr.sort((a, b) => b - a);
 }
 
+function copySorted(arr)
+{
+    // Or: return arr.slice().sort();
+    return Array.from(arr).sort();
+}
+
 let arr = [5, 2, 1, -10, 8];
 
 console.log(`Before: ${arr}`);
 sortDescending(arr);
 console.log(`After: ${arr}`);
+
+let new_arr = ["HTML", "JavaScript", "CSS"];
+console.log(`Before: ${new_arr}`);
+console.log(`After: ${new_arr} (original) ${copySorted(new_arr)} (sorted)`)
