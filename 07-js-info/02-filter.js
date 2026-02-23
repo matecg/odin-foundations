565b76ba304421b1f90cb91355ce31ad3d274f68
Gody <250132528+matecg@users.noreply.github.com>
2026-02-23 11:25:27 -0300

    Add exerc 03 solution with function to filter in place

--- 07-js-info/02-filter.js
+++ 07-js-info/02-filter.js
@@ -3,4 +3,16 @@ function filterRange(arr, min, max)
     return arr.filter((num) => num >= min && num <= max);
 }
 
+function filterRangeInPlace(arr, min, max)
+{
+    for (let i = arr.length; i > 0; i--)
+    {
+        const next = arr.pop();
+        if (next >= min && next <= max) arr.unshift(next);   
+    }
+}
+
 console.log(filterRange([5, 3, 8, 1], 1, 4));
+let a = [5, 3, 8, 1];
+filterRangeInPlace(a, 1, 4)
+console.log(a);
