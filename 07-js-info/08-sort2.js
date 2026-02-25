9cd622b05f3bfbb760b358a93dc29a00420a9203
Gody <250132528+matecg@users.noreply.github.com>
2026-02-25 17:26:54 -0300

    Add 'Get average age' exercise solution

--- 07-js-info/08-sort2.js
+++ 07-js-info/08-sort2.js
@@ -1,6 +1,6 @@
 let john = { name: "John", age: 25 };
 let pete = { name: "Pete", age: 30 };
-let mary = { name: "Mary", age: 28 };
+let mary = { name: "Mary", age: 29 };
 
 let arr = [ pete, john, mary ];
 
@@ -11,6 +11,13 @@ alert(arr[0].name); // John
 alert(arr[1].name); // Mary
 alert(arr[2].name); // Pete
 
+alert( getAverageAge(arr) ); // 28
+
 function sortByAge(arr) {
     arr.sort((a, b) => a.age - b.age);
+}
+
+function getAverageAge(arr) {
+    const sum = arr.reduce((acc, cur) => acc += cur.age, 0)
+    return sum / arr.length;
 }
